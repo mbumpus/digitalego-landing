3d8ad12139ef8e8c08b5435f3342dcd72d9ad375
AI Cabinet Method LLC <mike.bumpus@outlook.com>
2025-06-10 19:59:17 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
@@ -6,7 +5,17 @@ const nextConfig = {
       {
         source: '/socials',
         destination: 'https://bio.fm/socials-6',
-        permanent: true, // Use false if this might change in the future
+        permanent: true,
+      },
+      {
+        source: '/professor',
+        destination: '/coming-soon/professor',
+        permanent: false,
+      },
+      {
+        source: '/development',
+        destination: '/coming-soon/development',
+        permanent: false,
       },
     ];
   },
